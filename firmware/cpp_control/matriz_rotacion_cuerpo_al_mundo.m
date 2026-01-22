38dab78f02370356d34cf0c4400c221b43b1cc33
Emanuel López <loezemanuel8@gmail.com>
2026-01-22 00:10:30 -0300

    Delete firmware/cpp_control directory

--- firmware/cpp_control/matriz_rotacion_cuerpo_al_mundo.m
+++
@@ -1,8 +0,0 @@
-function R = matriz_rotacion_cuerpo_al_mundo(roll, pitch, yaw)
-cph = cos(roll); sph = sin(roll);
-cth = cos(pitch); sth = sin(pitch);
-cps = cos(yaw); sps = sin(yaw);
-R = [ cps*cth,  cps*sth*sph - sps*cph,  cps*sth*cph + sps*sph;
-      sps*cth,  sps*sth*sph + cps*cph,  sps*sth*cph - cps*sph;
-      -sth,     cth*sph,                cth*cph ];
-end
